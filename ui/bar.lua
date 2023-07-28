2e0e247790aadef5d6d3e90b0bbe4f2da6d9afcb
Rony Lee <rongyil33@gmail.com>
2023-07-28 15:46:20 +0800

    Rename files

--- ui/bar.lua
+++ ui/bar.lua
@@ -13,7 +13,7 @@ mytextclock = wibox.widget.textclock()
 
 screen.connect_signal("request::desktop_decoration", function(s)
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ " ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", " ﭮ ", "  " }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
