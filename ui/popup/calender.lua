a8a03e95273e94117d6132ef5559943d7c50e17d
Rony Lee <rongyil33@gmail.com>
2023-07-30 16:21:08 +0800

    Add margin to calender popup

--- ui/popup/calender.lua
+++ ui/popup/calender.lua
@@ -80,7 +80,7 @@ local calendar = awful.popup({
     border_width = 2,
     border_color = beautiful.bg_normal,
     placement = function(c)
-        (awful.placement.top_left)(c, { margins = { top = dpi(45), left = dpi(15) } })
+        (awful.placement.top_left)(c, { margins = { top = dpi(70), left = dpi(15) } })
     end,
     ontop = true,
 })
