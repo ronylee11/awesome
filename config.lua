b51b5f385114f7bc707f450ae631b3ee867d8e1d
Rony Lee <rongyil33@gmail.com>
2023-07-31 23:22:10 +0800

    Change mousemenu commands

--- config.lua
+++ config.lua
@@ -1,5 +1,4 @@
 local config = {}
-local gears = require("gears")
 
 config.apps = {
     terminal = "alacritty",
@@ -20,12 +19,8 @@ config.actions = {
     lock = "slock",
     -- screenshot
     screenshot = "scrot --multidisp --exec 'mv $f ".. os.getenv("HOME") .. "/Pictures/Screenshots/ && xdg-open ".. os.getenv("HOME") .. "/Pictures/Screenshots/$f'",
-    screenshot_area = "scrot --select --freeze --exec 'mv $f ".. os.getenv("HOME") .. "/Pictures/Screenshots/ && xdg-open ".. os.getenv("HOME") .. "/Pictures/Screenshots/$f'",
-    screenshot_window = "scrot --focused --border --exec 'mv $f ".. os.getenv("HOME") .. "/Pictures/Screenshots/ && xdg-open ".. os.getenv("HOME") .. "/Pictures/Screenshots/$f'",
-    screenshot_delay = "scrot --delay 5 --exec 'mv $f ".. os.getenv("HOME") .. "/Pictures/Screenshots/ && xdg-open ".. os.getenv("HOME") .. "/Pictures/Screenshots/$f'",
     -- other
-    show_album = "sxiv -t ".. os.getenv("HOME") .. "/Pictures",
-    --show_album = "alacritty",
+    show_album = "gthumb ".. os.getenv("HOME") .. "/Pictures",
 }
 
 return config
